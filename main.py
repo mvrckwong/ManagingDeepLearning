28f4c76e6f8ccaf50b4ff2806701a02819deb901
Maverick Wong <mvrck.wong@gmail.com>
2023-02-14 21:55:55 +0800

    update

--- main.py
+++ main.py
@@ -1,23 +1,138 @@
-import tensorflow as tf  # deep learning library. Tensors are just multi-dimensional arrays
+import os
+import random
 
-# mnist = tf.keras.datasets.mnist  # mnist is a dataset of 28x28 images of handwritten digits and their labels
-# (x_train, y_train),(x_test, y_test) = mnist.load_data()  # unpacks images to x_train/x_test and labels to y_train/y_test
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from tqdm.auto import tqdm
 
-# x_train = tf.keras.utils.normalize(x_train, axis=1)  # scales data between 0 and 1
-# x_test = tf.keras.utils.normalize(x_test, axis=1)  # scales data between 0 and 1
+# Ensure deterministic behavior
+torch.backends.cudnn.deterministic = True
+random.seed(hash("setting random seeds") % 2**32 - 1)
+np.random.seed(hash("improves reproducibility") % 2**32 - 1)
+torch.manual_seed(hash("by removing stochasticity") % 2**32 - 1)
+torch.cuda.manual_seed_all(hash("so runs are repeatable") % 2**32 - 1)
 
-# model = tf.keras.models.Sequential()  # a basic feed-forward model
-# model.add(tf.keras.layers.Flatten())  # takes our 28x28 and makes it 1x784
-# model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))  # a simple fully-connected layer, 128 units, relu activation
-# model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))  # a simple fully-connected layer, 128 units, relu activation
-# model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))  # our output layer. 10 units for 10 classes. Softmax for probability distribution
+# Device configuration
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# model.compile(optimizer='adam',  # Good default optimizer to start with
-# 				loss='sparse_categorical_crossentropy',  # how will we calculate our "error." Neural network aims to minimize loss.
-# 				metrics=['accuracy'])  # what to track
+# remove slow mirror from list of MNIST mirrors
+torchvision.datasets.MNIST.mirrors = [mirror for mirror in torchvision.datasets.MNIST.mirrors
+                                      if not mirror.startswith("http://yann.lecun.com")]
 
-# model.fit(x_train, y_train, epochs=3)  # train the model
+import wandb
 
-# val_loss, val_acc = model.evaluate(x_test, y_test)  # evaluate the out of sample data with model
-# print(val_loss)  # model's loss (error)
-# print(val_acc)  # model's accuracy
+def make(config):
+    # Make the data
+    train, test = get_data(train=True), get_data(train=False)
+    train_loader = make_loader(train, batch_size=config.batch_size)
+    test_loader = make_loader(test, batch_size=config.batch_size)
+
+    # Make the model
+    model = ConvNet(config.kernels, config.classes).to(device)
+
+    # Make the loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=config.learning_rate)
+    
+    return model, train_loader, test_loader, criterion, optimizer
+def train_log(loss, example_ct, epoch):
+    # Where the magic happens
+    wandb.log({"epoch": epoch, "loss": loss}, step=example_ct)
+    print(f"Loss after {str(example_ct).zfill(5)} examples: {loss:.3f}")
+    
+def test(model, test_loader):
+    model.eval()
+
+    # Run the model on some test examples
+    with torch.no_grad():
+        correct, total = 0, 0
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+        print(f"Accuracy of the model on the {total} " +
+              f"test images: {correct / total:%}")
+        
+        wandb.log({"test_accuracy": correct / total})
+
+    # Save the model in the exchangeable ONNX format
+    torch.onnx.export(model, images, "model.onnx")
+    wandb.save("model.onnx")
+
+def train(model, loader, criterion, optimizer, config):
+    # Tell wandb to watch what the model gets up to: gradients, weights, and more!
+    wandb.watch(model, criterion, log="all", log_freq=10)
+
+    # Run training and track with wandb
+    total_batches = len(loader) * config.epochs
+    example_ct = 0  # number of examples seen
+    batch_ct = 0
+    for epoch in tqdm(range(config.epochs)):
+        for _, (images, labels) in enumerate(loader):
+
+            loss = train_batch(images, labels, model, optimizer, criterion)
+            example_ct +=  len(images)
+            batch_ct += 1
+
+            # Report metrics every 25th batch
+            if ((batch_ct + 1) % 25) == 0:
+                train_log(loss, example_ct, epoch)
+
+
+def train_batch(images, labels, model, optimizer, criterion):
+    images, labels = images.to(device), labels.to(device)
+    
+    # Forward pass ➡
+    outputs = model(images)
+    loss = criterion(outputs, labels)
+    
+    # Backward pass ⬅
+    optimizer.zero_grad()
+    loss.backward()
+
+    # Step with optimizer
+    optimizer.step()
+
+    return loss
+
+config = dict(
+    epochs=5,
+    classes=10,
+    kernels=[16, 32],
+    batch_size=128,
+    learning_rate=0.005,
+    dataset="MNIST",
+    architecture="CNN")
+
+
+
+
+def model_pipeline(hyperparameters):
+
+    # tell wandb to get started
+    with wandb.init(project="pytorch-demo", config=hyperparameters):
+      # access all HPs through wandb.config, so logging matches execution!
+      config = wandb.config
+
+      # make the model, data, and optimization problem
+      model, train_loader, test_loader, criterion, optimizer = make(config)
+      print(model)
+
+      # and use them to train the model
+      train(model, train_loader, criterion, optimizer, config)
+
+      # and test its final performance
+      test(model, test_loader)
+
+    return model
+
+
+# Build, train and analyze the model with the pipeline
+model = model_pipeline(config)
